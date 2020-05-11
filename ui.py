235e7d767cb414db2afbc885f88e1a5081940639
Krishna Chandu <kchand23@uic.edu>
2020-05-11 10:19:55 -0500

    Minor bug fixes in UI

--- ui.py
+++ ui.py
@@ -9,10 +9,12 @@ heroes = bp.get_heroes()
 window = tk.Tk()
 
 
+
+
+#Dropdown menu for selecting heroes. 
 variable = tk.StringVar(window)
 variable.set(heroes[0]) # default value
-
-w = tk.OptionMenu(window, variable, *heroes)
-w.pack()
+hero_dropdown = tk.OptionMenu(window, variable, *heroes)
+hero_dropdown.pack()
 
 window.mainloop()
